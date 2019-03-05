246423e9ebbd667d3a6218c800d64a0af397b012
Connie Leung <cleung334@gmail.com>
2019-03-06 07:51:49 +0800

    feat: render and style all letters
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { EMPTY, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-ouija-board';
+export class AppComponent implements OnInit {
+    @ViewChildren('letters')
+    allLetters: QueryList<any>;
+
+    title = 'Yolo Brolo Ouija Board';
+
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    constructor(titleService: Title) {
+        titleService.setTitle('Ng Ouija Board');
+    }
+
+    ngOnInit() {
+        const toSpell$ = of('ponies');
+        const spelling$ = toSpell$.pipe(
+            mergeMap(word => {
+                console.log('word', word);
+                return EMPTY;
+            }),
+        );
+    }
+
+    findLetter(l: string) {
+        const span = this.allLetters.find(a => a.nativeElement.classList.contains(`letter-${l.toUpperCase()}`)) as HTMLSpanElement;
+        const rect = span.getBoundingClientRect();
+        const { top, left } = rect;
+        return { x: left, y: top };
+    }
 }
