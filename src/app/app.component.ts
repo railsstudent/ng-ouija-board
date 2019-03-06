df798181f7d2c483b0468a528793041b8eefd1f2
Connie Leung <cleung334@gmail.com>
2019-03-07 07:53:18 +0800

    feat: make the cursor moves over the letters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { of } from 'rxjs';
-import { mergeMap, tap } from 'rxjs/operators';
+import { Observable, of, timer } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -16,21 +16,31 @@ export class AppComponent implements OnInit {
 
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
+    spelling$: Observable<string>;
+    x = 0;
+    y = 60;
+
     constructor(titleService: Title) {
         titleService.setTitle('Ng Ouija Board');
     }
 
     ngOnInit() {
         const toSpell$ = of('ponies');
-        const spelling$ = toSpell$.pipe(
-            mergeMap(word => word.split('')),
-            tap(wordArray => console.log('wordArray', wordArray)),
-        );
+        toSpell$
+            .pipe(
+                mergeMap(word => word.split('')),
+                concatMap(letter => timer(200).pipe(map(() => letter))),
+            )
+            .subscribe(letter => {
+                const { x, y } = this.findLetter(letter);
+                this.x = x;
+                this.y = y;
+            });
     }
 
     findLetter(l: string) {
-        const span = this.allLetters.find(a => a.nativeElement.classList.contains(`letter-${l.toUpperCase()}`)) as HTMLSpanElement;
-        const rect = span.getBoundingClientRect();
+        const span = this.allLetters.find(a => a.nativeElement.classList.contains(`letter-${l.toUpperCase()}`)) as ElementRef;
+        const rect = span.nativeElement.getBoundingClientRect();
         const { top, left } = rect;
         return { x: left, y: top };
     }
