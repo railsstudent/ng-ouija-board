1e3a1a5e81f27de7398dbc0c7a46c322c197a893
Connie Leung <cleung334@gmail.com>
2019-03-12 08:30:39 +0800

    wip: cannot call tween function in scan.
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Observable, of, timer } from 'rxjs';
-import { concatMap, map, mergeMap } from 'rxjs/operators';
+import { concatMap, map, mergeMap, share, tap } from 'rxjs/operators';
+import { RxAnimationService } from './services';
 
 @Component({
     selector: 'app-root',
@@ -20,31 +21,54 @@ export class AppComponent implements OnInit {
     x = 0;
     y = 60;
 
+    x$: Observable<number>;
+    y$: Observable<number>;
+
     answer = '';
 
-    constructor(titleService: Title) {
+    tweenX = 0;
+    tweenY = 0;
+
+    constructor(titleService: Title, private animateService: RxAnimationService) {
         titleService.setTitle('Ng Ouija Board');
     }
 
     ngOnInit() {
-        const toSpell$ = of('ponies');
-        toSpell$
-            .pipe(
-                mergeMap(word => word.split('')),
-                concatMap(letter => timer(200).pipe(map(() => letter))),
-            )
-            .subscribe(letter => {
-                const { x, y } = this.findLetter(letter);
-                this.x = x;
-                this.y = y;
-                this.answer = this.answer + letter;
-            });
+        const toSpell$ = of('yolobrolo');
+
+        const spelling$ = toSpell$.pipe(
+            mergeMap(word => word.split('')),
+            concatMap(letter => timer(200).pipe(map(() => letter))),
+            map(letter => this.findLetter(letter)),
+            share(),
+        );
+
+        this.x$ = spelling$.pipe(
+            map(({ x }) => x),
+            concatMap(x => this.animateService.tween(this.tweenX, x, 1000)),
+            tap(dist => (this.tweenX = dist)),
+            tap(dist => console.log('How far does x tween?', dist)),
+        );
+
+        this.y$ = spelling$.pipe(
+            map(({ y }) => y),
+            concatMap(y => this.animateService.tween(this.tweenY, y, 1000)),
+            tap(dist => (this.tweenY = dist)),
+            tap(dist => console.log('How far does y tween?', dist)),
+        );
+
+        // .subscribe(letter => {
+        //     const { x, y } = this.findLetter(letter);
+        //     this.x = x;
+        //     this.y = y;
+        //     this.answer = this.answer + letter;
+        // });
     }
 
     findLetter(l: string) {
         const span = this.allLetters.find(a => a.nativeElement.classList.contains(`letter-${l.toUpperCase()}`)) as ElementRef;
         const rect = span.nativeElement.getBoundingClientRect();
         const { top, left } = rect;
-        return { x: left, y: top };
+        return { x: left, y: top } as { x: number; y: number };
     }
 }
