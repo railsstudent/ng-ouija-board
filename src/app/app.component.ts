629f2c99a6b6f8ed04d53345032f32dd620cc186
Connie Leung <cleung334@gmail.com>
2019-03-14 23:52:32 +0800

    refactor: inject document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { concat, Observable, of, timer } from 'rxjs';
 import { concatMap, map, mergeMap, share, tap } from 'rxjs/operators';
 import { RxAnimationService } from './services';
+
+interface Coordinates {
+    x: number;
+    y: number;
+}
 
 @Component({
     selector: 'app-root',
@@ -10,20 +16,15 @@ import { RxAnimationService } from './services';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    @ViewChildren('letters')
-    allLetters: QueryList<any>;
-
     title = 'Yolo Brolo Ouija Board';
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
-    spelling$: Observable<string>;
-    x$: Observable<string>;
-    y$: Observable<string>;
-
     tweenX = 0;
     tweenY = 0;
 
-    constructor(titleService: Title, private animateService: RxAnimationService) {
+    x$: Observable<number>;
+    y$: Observable<number>;
+
+    constructor(titleService: Title, private animateService: RxAnimationService, @Inject(DOCUMENT) private document: Document) {
         titleService.setTitle('Ng Ouija Board');
     }
 
@@ -41,24 +42,20 @@ export class AppComponent implements OnInit {
             map(({ x }) => x),
             concatMap(x => concat(timer(300), this.animateService.tween(this.tweenX, x, 1000))),
             tap(dist => (this.tweenX = dist)),
-            // tap(dist => console.log('How far does x tween?', dist)),
-            map(dist => `${dist}px`),
         );
 
         this.y$ = spelling$.pipe(
             map(({ y }) => y),
             concatMap(y => concat(timer(300), this.animateService.tween(this.tweenY, y, 1000))),
             tap(dist => (this.tweenY = dist)),
-            // tap(dist => console.log('How far does y tween?', dist)),
-            map(dist => `${dist}px`),
         );
     }
 
-    findLetter(l: string) {
-        const span = this.allLetters.find(a => a.nativeElement.classList.contains(`letter-${l.toUpperCase()}`)) as ElementRef;
-        const rect = span.nativeElement.getBoundingClientRect();
+    findLetter(l: string): Coordinates {
+        const $el = this.document.querySelector(`.letter-${l.toUpperCase()}`);
+        const rect = $el.getBoundingClientRect();
         // console.log('letter', l, rect);
         const { top, left } = rect;
-        return { x: left, y: top } as { x: number; y: number };
+        return { x: left, y: top };
     }
 }
